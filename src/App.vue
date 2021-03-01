74823b8f1be0124423d5f6a6c388ae0ec2b85607
tyu-ru <tyuru.tw.cpp@gmail.com>
2021-03-01 11:13:45 +0900

    feat: Add favicon & header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Kirafan Damage Distribution Plotter</h1>
+    <p>Recomended: PC or Tablet.</p>
+    <p>Instructions are at bottom of the page.</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
@@ -21,8 +18,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
